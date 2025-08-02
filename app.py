c45daa3dbb1db642b59ca3ebd9d529e240598e5b
anonymous Del Barco <61146153+maridasi47500@users.noreply.github.com>
2025-08-02 18:46:39 -0300

    scraping

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import request, Flask, render_template
+from webbrowser import open as surfweb
 
 app = Flask(__name__)
-def do_the_login():
+def do_the_login(name):
+    exec(name)
     print("hrhrh")
 
 @app.route('/hello/')
@@ -12,35 +14,35 @@ def hello(name=None):
 @app.route('/copycode', methods=['GET', 'POST'])
 def copycode(name=None):
     if request.method == 'POST':
-        return do_the_login()
+        return do_the_login(name)
     else:
         return render_template('copycode.html')
 
 @app.route('/executecommande', methods=['GET', 'POST'])
 def executecommande(name=None):
     if request.method == 'POST':
-        return do_the_login()
+        return do_the_login(name)
     else:
         return render_template('executecommande.html')
 
 @app.route('/callapis', methods=['GET', 'POST'])
 def callapis(name=None):
     if request.method == 'POST':
-        return do_the_login()
+        return do_the_login(name)
     else:
         return render_template('callapis.html')
 
 @app.route('/navigateweb', methods=['GET', 'POST'])
 def navigateweb(name=None):
     if request.method == 'POST':
-        return do_the_login()
+        surfweb(name)
     else:
         return render_template('navigateweb.html')
 
 @app.route('/editcode', methods=['GET', 'POST'])
 def editcode(name=None):
     if request.method == 'POST':
-        return do_the_login()
+        return do_the_login(name)
     else:
         return render_template('editcode.html')
 
